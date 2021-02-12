3f7a7eac4e9b4695dd484ee3ba350734cca370ae
Timur Zaripov <zaripov_t99@mail.ru>
2021-02-12 23:09:43 +0700

    Edit.v2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,11 @@
 
 fun main() {
-    val amount = 345256.45
+    val amount = 2.0
     val commission = 0.0075
-    val result: Double = amount * commission
+    var result: Double = amount * commission
 
     if (result < 35.00) {
-        println("Сумма перевод с комиссией: ${amount + 35.00} Руб")
-    } else {
-        println("Сумма перевод с комиссией: ${amount + result} Руб")
+        result = 35.00
     }
+    println("Сумма перевод с комиссией: ${amount + result} Руб")
 }
